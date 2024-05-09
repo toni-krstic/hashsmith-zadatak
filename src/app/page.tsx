0db4aeceb1ad1aca5deed9c5d734aba907b34e27
toni-krstic <krstic.toni.tk@gmail.com>
2024-05-09 20:24:29 +0200

    style up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,44 +18,43 @@ export default function HomePage() {
       <div className="w-full">
         <h3 className="p-2">{miners[19].name}</h3>
       </div>
-      <div className="flex flex-wrap gap-4">
+      <div className="flex flex-wrap gap-2">
         {formatedMiners.map((miners, i) => (
           <div
-            key={`${miners ? `${miners[`${i}`]?.pdu}--${miners[`${i}`]?.port}` : "s"}`}
+            key={`${miners ? `${miners[`${i}`]?.pdu}-${miners[`${i}`]?.port}` : "s"}`}
+            className="flex w-[420px] flex-wrap gap-2 rounded-md border-[1px] border-slate-400 p-4"
           >
-            <div className="flex flex-wrap gap-2 rounded-md border-[1px] border-slate-400 p-4">
-              {miners?.map((miner) => (
-                <div key={`${miner.pdu}-${miner.port}s`}>
-                  {miner.s === 10 ? (
-                    <div className="rounded-md bg-green-600 p-4">
-                      {miner.port}
-                    </div>
-                  ) : miner.s === 20 ? (
-                    <div className="rounded-md bg-blue-600 p-4">
-                      {miner.port}
-                    </div>
-                  ) : miner.s === 30 ? (
-                    <div className="rounded-md bg-yellow-600 p-4">
-                      {miner.port}
-                    </div>
-                  ) : miner.s === 40 ? (
-                    <div className="rounded-md bg-orange-600 p-4">
-                      {miner.port}
-                    </div>
-                  ) : miner.s === 50 ? (
-                    <div className="rounded-md bg-red-400 p-4">
-                      {miner.port}
-                    </div>
-                  ) : miner.s === 60 ? (
-                    <div className="rounded-md bg-red-600 p-4">
-                      {miner.port}
-                    </div>
-                  ) : (
-                    <></>
-                  )}
-                </div>
-              ))}
-            </div>
+            {miners?.map((miner) => (
+              <div key={`${miner.pdu}-${miner.port}s`}>
+                {miner.s === 10 ? (
+                  <div className="flex h-8 w-8 items-center justify-center rounded-md bg-green-600 p-4">
+                    {miner.port}
+                  </div>
+                ) : miner.s === 20 ? (
+                  <div className="flex h-8 w-8 items-center justify-center rounded-md bg-blue-600 p-4">
+                    {miner.port}
+                  </div>
+                ) : miner.s === 30 ? (
+                  <div className="flex h-8 w-8 items-center justify-center rounded-md bg-yellow-600 p-4">
+                    {miner.port}
+                  </div>
+                ) : miner.s === 40 ? (
+                  <div className="flex h-8 w-8 items-center justify-center rounded-md bg-orange-600 p-4">
+                    {miner.port}
+                  </div>
+                ) : miner.s === 50 ? (
+                  <div className="flex h-8 w-8 items-center justify-center rounded-md bg-red-400 p-4">
+                    {miner.port}
+                  </div>
+                ) : miner.s === 60 ? (
+                  <div className="flex h-8 w-8 items-center justify-center rounded-md bg-red-600 p-4">
+                    {miner.port}
+                  </div>
+                ) : (
+                  <></>
+                )}
+              </div>
+            ))}
           </div>
         ))}
       </div>
