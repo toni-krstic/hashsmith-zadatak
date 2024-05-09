4711ab9e58082eaf386797822cdaa06dfa6362a3
toni-krstic <krstic.toni.tk@gmail.com>
2024-05-09 19:26:07 +0200

    miner status color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,23 @@ export default function HomePage() {
             {miners.pdu === miners.port ? (
               <div className="flex border-b border-slate-100">
                 {getMiner(miners.pdu).map((miner) => (
-                  <div key={`${miner.pdu}-${miner.port}s`}>{miner.port}</div>
+                  <div key={`${miner.pdu}-${miner.port}s`}>
+                    {miner.s === 10 ? (
+                      <div className="bg-green-400">{miner.port}</div>
+                    ) : miner.s === 20 ? (
+                      <div className="bg-blue-400">{miner.port}</div>
+                    ) : miner.s === 30 ? (
+                      <div className="bg-yellow-400">{miner.port}</div>
+                    ) : miner.s === 40 ? (
+                      <div className="bg-orange-400">{miner.port}</div>
+                    ) : miner.s === 50 ? (
+                      <div className="bg-red-400">{miner.port}</div>
+                    ) : miner.s === 60 ? (
+                      <div className="bg-red-600">{miner.port}</div>
+                    ) : (
+                      <></>
+                    )}
+                  </div>
                 ))}
               </div>
             ) : (
